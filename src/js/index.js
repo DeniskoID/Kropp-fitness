ba39c83eb99bf211bef73011636b5418107e3922
Denis <80954428+DeniskoID@users.noreply.github.com>
2024-01-13 15:41:49 +0300

    init

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+// import lenis from './vendor/lenis.js';
+// lenis.start();
+
+// import buttonToTop from './modules/to-top-button.js';
+// buttonToTop();
+
+// import swiper1 from './vendor/hero-swiper.js';
+// swiper1.init();
+
+// import initMobileSwiper from './vendor/services-swiper.js';
+// initMobileSwiper();
+
+// import portfolioSwiper from './vendor/portfolio-swiper.js';
+// portfolioSwiper.init();
+
+// import menuBgAnimation from './modules/nav-bg.js';
+// menuBgAnimation();
+
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+
+// import textCollapser from './modules/text-collapser.js';
+// textCollapser('.text-collapser', '.about__description', '195px');
+// textCollapser('.portfolio__collapser', '.portfolio__collaps-helper', '500px');
+
+// import servicesCardHover from './modules/gsap-interactions.js';
+// servicesCardHover();
+
+// import modals from './modules/modals.js';
+// modals();
+
+// import toAnchor from './modules/anchor-navigation.js';
+// toAnchor();
+
+// import showLazyMap from './modules/lazy-ymap.js';
+// showLazyMap();
